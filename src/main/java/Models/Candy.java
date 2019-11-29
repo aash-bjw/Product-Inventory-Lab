7e96dc46aa451c6fa33be57aa730353d90ea97b1
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-11-29 09:31:49 -0500

    Constructor & Getters & Setters

--- src/main/java/Models/Candy.java
+++ src/main/java/Models/Candy.java
@@ -1,4 +1,43 @@
 package Models;
 
 public class Candy {
+
+    private String brand;
+    private Integer quantity;
+    private Float price;
+
+
+    public Candy(String brand, Integer quantity, Float price) {
+        this.brand = brand;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Candy() {
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
 }
--- src/main/java/Models/Soda.java
+++ src/main/java/Models/Soda.java
@@ -1,4 +1,61 @@
 package Models;
 
 public class Soda {
+    private String brand;
+    private Integer flavor;
+    private Integer size;
+    private Integer quantity;
+    private Float price;
+
+    public Soda(String brand, Integer flavor, Integer size, Integer quantity, Float price) {
+        this.brand = brand;
+        this.flavor = flavor;
+        this.size = size;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Soda() {
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(Integer flavor) {
+        this.flavor = flavor;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
 }
